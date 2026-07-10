d6b48c747704c9afce789ec6f54bf73d4e8cc387
Hendrike Heydenreich <mail@gretalee.de>
2026-07-10 15:27:31 +0200

    Add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Verdana', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
